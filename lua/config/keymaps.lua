fa7f6c9f1859696265868a55ec0676ae7da6a686
Mario Schillermann <mario.schillermann@gmail.com>
2024-12-29 19:01:51 +0100

    add keymaps for buffer navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,16 @@ vim.g.mapleader = " "
 
 -- File Explorer
 keymap("n", "<leader>e", ":Lexplore <CR>", opts)
--- Jump to Window
+-- Jump To Window
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
--- Resize window
+-- Resize Window
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Buffer Navigation
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
