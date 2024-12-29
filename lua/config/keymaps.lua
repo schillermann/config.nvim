093391c57dda6204d996c56b615d512e9fb2e66e
Mario Schillermann <mario.schillermann@gmail.com>
2024-12-29 18:41:00 +0100

    change keymaps for jumping between windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,10 @@
-local opts = {}
+local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader = " "
 
 keymap("n", "<leader>e", ":Lexplore <cr>", opts)
+-- Windows
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
