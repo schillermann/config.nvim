eb2cbfcefd16fc8fdd66dc8de168b59d118f0479
Mario Schillermann <mario.schillermann@gmail.com>
2025-01-19 21:04:39 +0100

    create command to close current buffer without closing split window

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command("ResizeWindow", function(opts)
   vim.cmd("vertical resize " .. opts.args)
 end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("BufferCloseCurrent", function()
+  vim.cmd("bp|bd #")
+end, { nargs = 0 })
