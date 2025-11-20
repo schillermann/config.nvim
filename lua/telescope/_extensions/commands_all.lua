6f0964176cf868383f0b243ec691f61821542d89
Mario Schillermann <mario.schillermann@gmail.com>
2025-11-20 14:35:29 +0100

    create picker for commands_all
    Telescope commands_all outputs a list of commands from multiple sources.

---
+++ lua/telescope/_extensions/commands_all.lua
@@ -0,0 +1,20 @@
+local command_list = require("telescope._extensions.command_list")
+
+return require("telescope").register_extension({
+  exports = {
+    commands_all = function(opts)
+      local filenames = { "commands_options.json", "commands_ex.json" }
+      local title = "All Commands"
+      local column_widths = { 15, 20, 0 }
+
+      local picker = command_list.multi_picker(filenames, title, column_widths, opts)
+
+      if picker == nil then
+        vim.notify("Missing picker for `Telescope commands_all`",
+          vim.log.levels.ERROR)
+        return
+      end
+      picker:find()
+    end
+  },
+})
