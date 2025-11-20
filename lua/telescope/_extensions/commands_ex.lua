b77a16a37793610c034705cb695d2a43f8ae169f
Mario Schillermann <mario.schillermann@gmail.com>
2025-11-20 14:43:03 +0100

    adjust telescope command_ex with error message

--- lua/telescope/_extensions/commands_ex.lua
+++ lua/telescope/_extensions/commands_ex.lua
@@ -3,7 +3,17 @@ local command_list = require("telescope._extensions.command_list")
 return require("telescope").register_extension({
   exports = {
     commands_ex = function(opts)
-      command_list.picker("commands_ex.json", "Ex-Commands Index", { 15, 17, 0 }, opts):find()
+      local filename = "commands_ex.json"
+      local title = "Ex-Commands Index"
+      local column_widths = { 15, 17, 0 }
+
+      local picker = command_list.picker(filename, title, column_widths, opts)
+      if picker == nil then
+        vim.notify("Missing picker for `Telescope commands_ex`",
+          vim.log.levels.ERROR)
+        return
+      end
+      picker:find()
     end
   },
 })
