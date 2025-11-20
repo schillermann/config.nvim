5203f94357e5d327e32b4f809a3ffc38c0f39a28
Mario Schillermann <mario.schillermann@gmail.com>
2025-11-20 14:43:55 +0100

    adjust telescope commands_options with error message

--- lua/telescope/_extensions/commands_options.lua
+++ lua/telescope/_extensions/commands_options.lua
@@ -3,7 +3,18 @@ local command_list = require("telescope._extensions.command_list")
 return require("telescope").register_extension({
   exports = {
     commands_options = function(opts)
-      command_list.picker("commands_options.json", "Commands Options", { 25, 0 }, opts):find()
+      local filename = "commands_options.json"
+      local title = "Commands Options"
+      local column_widths = { 25, 0 }
+
+      local picker = command_list.picker(filename, title, column_widths, opts)
+
+      if picker == nil then
+        vim.notify("Missing picker for `Telescope commands_options`",
+          vim.log.levels.ERROR)
+        return
+      end
+      picker:find()
     end
   },
 })
