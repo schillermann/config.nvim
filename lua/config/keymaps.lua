e0bc1e60d015a0723692c48d3cef38385fefaca8
Mario Schillermann <mario.schillermann@gmail.com>
2026-02-18 18:40:31 +0100

    simplifies telescope live grep shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -60,7 +60,7 @@ map.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 -- Package Telescope
 local builtin = require("telescope.builtin")
 map.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-map.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+map.set("n", "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
 map.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
 map.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 -- Package Copilot Chat
