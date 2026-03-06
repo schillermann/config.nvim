1dd70fa9fc2c19d7b62138a16560064c8a1d1cc6
Mario Schillermann <mario.schillermann@gmail.com>
2026-03-06 11:50:01 +0100

    create command to close other buffers

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -5,3 +5,7 @@ end, { nargs = 1 })
 vim.api.nvim_create_user_command("BufferCloseCurrent", function()
   vim.cmd("bp|bd #")
 end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("BufferCloseOthers", function()
+  vim.cmd("%bd|e#|bd#")
+end, { nargs = 0 })
