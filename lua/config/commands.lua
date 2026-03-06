8b9eb6d9200167f4cb92e8cca5efee5366b1e21d
Mario Schillermann <mario.schillermann@gmail.com>
2026-03-06 12:22:37 +0100

    replace Vimscript One-Liner `bp|bd #` with lua logic
    If you only have one buffer open, `bd #` will often close the entire window or even exit Neovim.

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -2,9 +2,20 @@ vim.api.nvim_create_user_command("ResizeWindow", function(opts)
   vim.cmd("vertical resize " .. opts.args)
 end, { nargs = 1 })
 
-vim.api.nvim_create_user_command("BufferCloseCurrent", function()
-  vim.cmd("bp|bd #")
-end, { nargs = 0 })
+vim.api.nvim_create_user_command('BufferCloseCurrent', function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local listed_bufs = vim.fn.getbufinfo({ buflisted = 1 })
+
+  if #listed_bufs <= 1 then
+    -- If it's the last buffer, create a new one first so the UI doesn't collapse
+    vim.cmd('enew')
+    vim.cmd('bd ' .. bufnr)
+  else
+    -- Otherwise, go to the previous buffer and delete the old one
+    vim.cmd('bp')
+    vim.cmd('bd ' .. bufnr)
+  end
+end, {})
 
 vim.api.nvim_create_user_command('BufferCloseOthers', function()
   local current_buf = vim.api.nvim_get_current_buf()
