a55465f97ff8271c38cd590164e5e36c564b1716
Mario Schillermann <mario.schillermann@gmail.com>
2026-03-06 12:12:05 +0100

    replace classic vim command calls with lua logic
    Vimscript One-Liner `:%bd|e#|bd#` is a "blunt instrument."
    Because it deletes everything first, you can lose state for special UI windows (like file trees or terminal toggles) that might have been open. It also resets things like your jump list for the other buffers.

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -6,6 +6,14 @@ vim.api.nvim_create_user_command("BufferCloseCurrent", function()
   vim.cmd("bp|bd #")
 end, { nargs = 0 })
 
-vim.api.nvim_create_user_command("BufferCloseOthers", function()
-  vim.cmd("%bd|e#|bd#")
-end, { nargs = 0 })
+vim.api.nvim_create_user_command('BufferCloseOthers', function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local buffers = vim.api.nvim_list_bufs()
+
+  for _, bufnr in ipairs(buffers) do
+    if bufnr ~= current_buf and vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted then
+      vim.api.nvim_buf_delete(bufnr, { force = false })
+    end
+  end
+end, { desc = 'Close all buffers except the current one' })
+
