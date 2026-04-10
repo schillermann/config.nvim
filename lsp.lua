a505bf68cb71e9fe5613d4ca2e56614280711529
Mario Schillermann <mario.schillermann@gmail.com>
2026-04-10 10:38:24 +0200

    refactor: centralize external dependencies in `mason-tool-installer`
    - Move `lua_ls` from mason-lspconfig to `mason-tool-installer`
    - Keep all external dependencies in a single list for easier management

--- lsp.lua
+++ lsp.lua
@@ -1,13 +1,10 @@
 require("mason").setup()
 -- Handles LSPs only
-require("mason-lspconfig").setup {
-  automatic_enable = {
-    "lua_ls",
-  }
-}
+require("mason-lspconfig").setup()
 -- Handles everything else (formatters, linters, debuggers)
 require("mason-tool-installer").setup({
   ensure_installed = {
+    "lua_ls",
     "stylua",
   },
 })
